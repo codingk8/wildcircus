53fc6d2910b3cba286616c0555e65e18c3c3b6c9
codingk8 <codingk8@gmail.com>
2018-01-08 22:19:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<p align="center"><img src="https://s26.postimg.org/hrlfmruu1/wondersushilogotext250.png" alt="Wild circus ReadMe logo")</p>
+
+# 🇬🇧 The Wild circus project
+
+## The Wild circus project ...
+
+xxxxx
+
+> Hosted demo website: [Wild circus](https://codingk8.github.io/wildcircus/)
+
+(More technical elements soon 😉)
+
+*Disclaimer: this project has no commercial purpose.*
+
+# 🇫🇷 Le projet Wild Circus
+
+xxx
+
+Avec l'aide de Canva pour les images
+Les liens vers le Cirque du soleil...
+
+> Site de démo hébergé : [Wild circus](https://codingk8.github.io/wildcircus/)
+
+*Avertissement : ce projet n'a pas d'objectif commercial.*
---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+/* Codingk8 - Wild circus 🎪 project - The project that got me into my coding bootcamp */
+
+$(".imagecentre1").on("mouseenter", function() {
+		$(this).css("opacity", 0.5);
+});
+
+$(".imagecentre1").on("mouseleave", function() {
+		$(this).css("opacity", 1);
+});
+
+$(".imagecentre").on("mouseenter", function() {
+	$(this).css("opacity", 0.5);
+});
+
+$(".imagecentre").on("mouseleave", function() {
+	$(this).css("opacity", 1);
+});
+
+$(".footer").on("mouseenter", function() {
+	$(this).css("background-color", "#66cccc");
+})
+
+$(".footer").on("mouseleave", function() {
+	$(this).css("background-color", "#336666");
+})
+
+$(".aside").on("mouseenter", function() {
+	$(this).css("border-color", "#cc3333");
+})
+
+$(".aside").on("mouseleave", function() {
+	$(this).css("border-color", "#ffcc00");
+})
+
+$(".joke").on("mouseenter", function() {
+	$(this).html("This is a joke :-) But they are really wonderful!");
+	$(this).css("background-color", "#66cccc");
+})
+
+$(".joke").on("mouseleave", function() {
+	$(this).html("Learn more about our wonderful attractions!");
+	$(this).css("background-color", "#cc3333");
+})
